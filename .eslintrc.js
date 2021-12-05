7cae966b77429614b3cb8ec85197821a820bc8ef
Patrick Morais <ppternunes@gmail.com>
2021-12-05 15:26:18 -0300

    typeorm config / postgres connection / CreateUsers migration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,17 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 13,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   rules: {
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'import/prefer-default-export': 0,
+    'class-methods-use-this': 0,
   },
 };
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
+import 'reflect-metadata';
 import express from 'express';
 
+import './database';
+
 const app = express();
+
+app.get('/', (request, response) => {
+  return response.send('Hello World!');
+});
+
+app.post('/', (request, response) => {
+  return response.send('Hello World! - POST');
+});
 
 app.listen(3000, () => {
   console.log('Server is running!');
