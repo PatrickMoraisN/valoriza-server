44347b9aae1cc448fdea23a2bb3672c39cd82174
Patrick Morais <ppternunes@gmail.com>
2021-12-05 19:59:25 -0300

    create tag service

---
+++ src/services/CreateTagService.ts
@@ -0,0 +1,25 @@
+import { getCustomRepository } from 'typeorm';
+import { TagsRepositories } from '../repositories/TagsRepositories';
+
+class CreateTagService {
+  async execute(name: string) {
+    const tagRepositories = getCustomRepository(TagsRepositories);
+
+    if (!name) {
+      throw new Error('Incorrect name!');
+    }
+
+    const tagAlreadyExists = await tagRepositories.findOne({ name });
+
+    if (tagAlreadyExists) {
+      throw new Error('Tag already exists!');
+    }
+
+    const tag = tagRepositories.create({ name });
+    await tagRepositories.save(tag);
+
+    return tag;
+  }
+}
+
+export { CreateTagService };
