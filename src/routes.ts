95df3119690a6a0f75c5e688699e47da41856021
Patrick Morais <ppternunes@gmail.com>
2021-12-06 12:23:13 -0300

    add login route

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateTagController } from './controllers/CreateTagController';
 import { CreateUserController } from './controllers/CreateUserController';
 import { EnsureAdmin } from './middlewares/EnsureAdmin';
@@ -7,9 +8,11 @@ const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
 const ensureAdmin = new EnsureAdmin();
 
 router.post('/users', createUserController.handle);
 router.post('/tags', ensureAdmin.handle, createTagController.handle);
+router.post('/login', authenticateUserController.handle);
 
 export { router };
